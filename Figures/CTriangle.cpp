a2dcdc044751804cc57509d38f4aab50ba78e23b
Bassem Sherief <108496148+BassemAdam@users.noreply.github.com>
2022-12-25 22:37:57 +0200

    editing something

--- Figures/CTriangle.cpp
+++ Figures/CTriangle.cpp
@@ -23,9 +23,9 @@ bool CTriangle::IsInside(int x, int y) {
 }
 void CTriangle::Move(int x, int y) {
 	int xcenter = (Corner1.x + Corner2.x + Corner3.x) / 3; //getting intersection of medians of triangle
-	int ycenter = (Corner1.y + Corner2.y + Corner3.y) / 3;
+	int ycenter = (Corner1.y + Corner2.y + Corner3.y) / 3; 
 	int xtranslate = xcenter - x;
-	int ytranslate = ycenter - x;
+	int ytranslate = ycenter - y; 
 	Corner1.x -= xtranslate;
 	Corner1.y -= ytranslate;
 	Corner2.x -= xtranslate;

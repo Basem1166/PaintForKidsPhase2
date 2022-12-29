49aa1cf27be1551aa4b39af81940576698ed8629
Alii-s <119129254+Alii-s@users.noreply.github.com>
2022-12-29 18:48:45 +0200

    sss

--- Actions/ChangeFillAction.cpp
+++ Actions/ChangeFillAction.cpp
@@ -59,7 +59,7 @@ void ChangeFillAction::Execute() {
 	ReadActionParameters();
 	Output* pOut = pManager->GetOutput();
 	CFigure* SelectedFigure = pManager->GetSelectedFigure();
-	OldGfxInfo = SelectedFigure->GetGfxInfo();
+
 	if (GetFillColour(ColorAct)) {
 		UI.isFilled = true;
 		SelectedFigure->ChngFillClr(UI.FillColor); //changing the fill color

d069dbaa18aedd558f1e08d86165861d6e9c4718
Alii-s <119129254+Alii-s@users.noreply.github.com>
2023-12-09 13:59:11 +0200

    Revert "lmfao we done nig*a"
    This reverts commit 25ca0a16c5c5708a93cae6aee742b6171764b075.

--- Figures/CFigure.h
+++ Figures/CFigure.h
@@ -9,12 +9,12 @@ class CFigure
 {
 protected:
 
-	static int count; // count of figures
+	static int count;
 	int ID;		//Each figure has an ID
 	bool Selected;	//true if the figure is selected.
 	GfxInfo FigGfxInfo;	//Figure graphis info
 	Point Center; //Center of the figure
-	bool Hidden; //Hidden status
+	bool Hidden;
 	/// Add more parameters if needed.
 	static int ArrOfColors[];
 
@@ -24,18 +24,18 @@ public:
 
 	CFigure(); //to allow zero argument constructor in children
 	CFigure(GfxInfo FigureGfxInfo);
-	void ResetArrOfClrs(); //Resets array of colours
-	static void resetID(); //delets all IDs
+	void ResetArrOfClrs();
+	static void resetID();
 	void SetSelected(bool s);	//select/unselect the figure
 	bool IsSelected() const;	//check whether fig is selected
-	bool IsHidden() const; //check if figure will be drawn
-	void SetHidden(bool) ; //set hide status
-	int GetID(); //returns id of figure
-	void SetID(int);// sets id of figure
-	Point GetCenter();//Returns center of figure
-	GfxInfo GetGfxInfo()const; //returns graphics info of figures
+	bool IsHidden() const;
+	void SetHidden(bool) ;
+	int GetID();
+	void SetID(int);
+	Point GetCenter();
+	GfxInfo GetGfxInfo()const;
 	virtual void Draw(Output* pOut) const  = 0 ;		//Draw the figure
-	void ChngGfxInfo(GfxInfo); //Change Graphics info
+	void ChngGfxInfo(GfxInfo);
 	void ChngDrawClr(color Dclr);	//changes the figure's drawing color
 	void ChngFillClr(color Fclr);	//changes the figure's filling color
 	virtual bool IsInside(int,int) = 0; //checks if the points clicked are inside a figure
@@ -54,8 +54,8 @@ public:
 	//converting color type to string to be able to write it in text files
 	string convertcolorToString(color color); //note i should have used those conversions static in load,save but when i did it gets errors
 	//converting string type to color to be able to read it from text files
-	color convertStringToColor(string color); //Converts string into a color type
-	virtual string GetFigureType()=0; //Get Figure type
+	color convertStringToColor(string color);
+	virtual string GetFigureType()=0;
 	
 };
 
